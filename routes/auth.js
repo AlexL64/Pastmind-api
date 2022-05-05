ed22182a11303b48d0c952aefedeb0985fd3d6f8
AlexL64 <alex.lucarin@gmail.com>
2022-05-05 17:10:31 +0200

    database test

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import emailValidator from 'email-validator';
+import mysql from 'mysql';
 dotenv.config();
 
 const router = express.Router();
-const KEY = process.env.API_KEY;
+const API_KEY = process.env.API_KEY;
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+var db = mysql.createConnection({
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_DATABASE
+  });
 
 router.post('/', (req, res) => {
 
-    if(req.query.key != KEY){
+    if(req.query.key != API_KEY){
         res.send({
             status: "Error",
             error: "Unknown api key"
@@ -23,7 +34,6 @@ router.post('/', (req, res) => {
         }else{
             var status = checkIdentity("username", login, password);
         }
-        
     }
 });
 

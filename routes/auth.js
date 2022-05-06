3f07f64895248ddea7fda8544e01e12b9750770c
AlexL64 <alex.lucarin@gmail.com>
2022-05-06 09:09:41 +0200

    database

--- routes/auth.js
+++ routes/auth.js
@@ -9,6 +9,7 @@ const API_KEY = process.env.API_KEY;
 const DB_HOST = process.env.DB_HOST;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_DATABASE = process.env.DB_DATABASE;
 
 var db = mysql.createConnection({
     host: DB_HOST,
